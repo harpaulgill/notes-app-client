92f4bbca5b43b55ebf3db67e872b7afdefc02fd1
Harpaul Gill <harpaul_gill@hotmail.com>
2017-12-30 12:32:09 -0800

    now displays all notes that have been added through the API

--- src/config.js
+++ src/config.js
@@ -2,6 +2,15 @@ export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   cognito: {
     USER_POOL_ID: "us-east-2_WFYUrUdGX",
-    APP_CLIENT_ID: "5ll3690fuof2kuid765usgifas"
+    APP_CLIENT_ID: "5ll3690fuof2kuid765usgifas",
+    REGION: "us-east-2",
+    IDENTITY_POOL_ID: "us-east-2:15ea9eb6-c9c2-45bd-be64-ece6f9a0ff31"
+  },
+  s3: {
+    BUCKET: "notes-upload9"
+  },
+  apiGateway: {
+    URL: "https://3fe40fcwk5.execute-api.us-east-2.amazonaws.com/prod",
+    REGION: "us-east-2"
   }
 };
