37523458ca73d433e6047b8406499a8b8e8cca5f
Harpaul Gill <harpaul_gill@hotmail.com>
2017-12-30 12:55:27 -0800

    can now add a new note and edit and save changes to existing notes

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 import AppliedRoute from "./components/AppliedRoute";
 
 export default ({ childProps }) =>
@@ -13,6 +14,7 @@ export default ({ childProps }) =>
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
